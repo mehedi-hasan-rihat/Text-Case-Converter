73ba68aa7d89c0f51e06b44cc8e50b4360e0d8d1
Mehedi Hasan Rihat <mehedi4794@gmail.com>
2025-08-07 10:05:15 +0600

    refactor: remove react-ga4 integration and Google Analytics initialization from App component - Initialization Google Analytics with script tag

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import React, { useEffect } from 'react';
-import ReactGA from 'react-ga4';
+import React from 'react';
 import TextCaseConverter from './components/TextCaseConverter';
 
 const App: React.FC = () => {
-  useEffect(() => {
-    // Initialize GA
-    ReactGA.initialize('G-1SW9LKSYJ9');
-    ReactGA.send({ hitType: 'pageview', page: window.location.pathname });
-  }, []);
-
   return <TextCaseConverter />;
 };
 
